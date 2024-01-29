bf7e55025da736c6f635a127a0a4ee3b9f0c5d74
Christian Adler <chris.adler@gmx.de>
2024-01-29 21:34:58 +0100

    use random ai

--- js/car.js
+++ js/car.js
@@ -15,8 +15,12 @@ class Car {
         this.color = color;
         this.damaged = false;
 
-        if (controlType !== 'DUMMY')
+        this.useBrain = controlType === 'AI';
+
+        if (controlType !== 'DUMMY') {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]); // last layer has 4 outputs (forward, backward, left, right)
+        }
         this.controls = new Controls(controlType);
     }
 
@@ -26,8 +30,19 @@ class Car {
             this.polygon = this.#createPolygon();
             this.damaged = this.#assessDamage(roadBorders, traffic);
         }
-        if (this.sensor)
+        if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(s => s == null ? 0 : 1 - s.offset);
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+            // console.log(outputs);
+
+            if (this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
+        }
     }
 
     #assessDamage(roadBorders, traffic) {
--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const ctx = canvas.getContext('2d');
 
 const road = new Road(canvas.width / 2, canvas.width * 0.9, 3);
 
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 3, "blue");
+// const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 3, "blue");
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 3, "blue");
 
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
