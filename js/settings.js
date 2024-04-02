adae0bcc5f78132b76dc47fef4601c94437da46e
Christian Adler <chris.adler@gmx.de>
2024-04-02 20:07:08 +0200

    implement some more race preparations

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 const Settings = {
     useCarImage: false,
-    numCars: 100, // Only low N if using img!
+    numCars: 10, // Only low N if using img!
     mutationFactor: 0.1,
     controlType: 'AI', // one of AI, KEYS, DUMMY
     worldGenerateTreesTryCount: 100, // 10 for quick
--- js/sim.js
+++ js/sim.js
@@ -1,7 +1,12 @@
 const carCanvas = window.document.getElementById('carCanvas');
-carCanvas.width = window.innerWidth;
+carCanvas.width = window.innerWidth - 330;
 carCanvas.height = window.innerHeight;
 const carCtx = carCanvas.getContext('2d');
+
+const networkCanvas = window.document.getElementById('networkCanvas');
+networkCanvas.width = 300;
+networkCanvas.height = window.innerHeight - 310;
+const networkCtx = networkCanvas.getContext('2d');
 
 const miniMapCanvas = window.document.getElementById('miniMapCanvas');
 miniMapCanvas.width = 300;
@@ -12,22 +17,24 @@ const miniMap = new MiniMap(miniMapCanvas, world.graph, 300);
 
 const useCarImg = Settings.useCarImage; // Only low N if using img!
 const N = Settings.numCars; // useCarImg ? 100 : 1000;
-const cars = generateCars(1, useCarImg, 'KEYS').concat(generateCars(N, useCarImg, 'AI'));
-const myCar = cars[0];
+const cars = generateCars(N, useCarImg);
+let bestCar = cars[0];
 const savedBrain = localStorage.getItem("bestBrain");
 if (savedBrain) {
     for (let i = 0; i < cars.length; i++) {
         const car = cars[i];
         car.brain = JSON.parse(savedBrain);
-        if (i > 1) // 0 == myCar -> 1 is saved brain - others mutate
+        if (i !== 0)
             NeuralNetwork.mutate(car.brain, Settings.mutationFactor);
     }
 }
 
+const traffic = [];
+
 let roadBorders;
 const target = world.markings.find(m => m instanceof Target);
 if (target) {
-    world.generateCorridor(myCar, target.center);
+    world.generateCorridor(bestCar, target.center);
     roadBorders = world.corridor.map(s => [s.p1, s.p2]);
 } else
     roadBorders = world.roadBorders.map(s => [s.p1, s.p2]);
@@ -35,40 +42,46 @@ if (target) {
 animate();
 
 function save() {
-    localStorage.setItem('bestBrain', JSON.stringify(myCar.brain));
+    localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain));
 }
 
 function discard() {
     localStorage.removeItem('bestBrain');
 }
 
-function generateCars(N, useCarImg, type) {
+function generateCars(N, useCarImg) {
     const startPoints = world.markings.filter(m => m instanceof Start);
     const startPoint = startPoints.length > 0 ? startPoints[0].center : new Point(100, 100);
     const dir = startPoints.length > 0 ? startPoints[0].directionVector : new Point(0, -1);
     const startAngle = -angle(dir) + Math.PI / 2;
     const cars = [];
     for (let i = 0; i < N; i++) {
-        const color = type === 'AI' ? getRandomColor() : 'purple';
-        const car = new Car(startPoint.x, startPoint.y, 30, 50, type, startAngle, 3, color, useCarImg);
+        const car = new Car(startPoint.x, startPoint.y, 30, 50, Settings.controlType, startAngle, 3, "purple", useCarImg);
         car.load(carInfo)
         cars.push(car);
     }
     return cars;
 }
 
-function animate() {
+function animate(time) {
+    for (const c of traffic) {
+        c.update(roadBorders, []);
+    }
     for (const car of cars) {
-        car.update(roadBorders, []);
+        car.update(roadBorders, traffic);
     }
 
+    const carsMaxFitness = Math.max(...cars.map(c => c.fitness));
+    bestCar = cars.find(c => c.fitness === carsMaxFitness);
+    if (!bestCar) bestCar = cars[0];
+
     world.cars = cars;
-    world.bestCar = myCar;
+    world.bestCar = bestCar;
 
     // follow car (but panning is no longer possible)
-    if (myCar) {
-        viewport.offset.x = -myCar.x;
-        viewport.offset.y = -myCar.y;
+    if (bestCar) {
+        viewport.offset.x = -bestCar.x;
+        viewport.offset.y = -bestCar.y;
     } else {
         console.log("no best car?");
     }
@@ -78,6 +91,15 @@ function animate() {
     world.draw(carCtx, viewPoint, false);
 
     miniMap.update(viewPoint);
+
+    for (const c of traffic) {
+        c.draw(carCtx);
+    }
+
+    networkCtx.lineDashOffset = -time / 50;
+    networkCtx.clearRect(0, 0, networkCanvas.width, networkCanvas.height);
+    if (bestCar)
+        Visualizer.drawNetwork(networkCtx, bestCar.brain);
 
     requestAnimationFrame(animate);
 }
