52ee9783a68c222a2ea9551df332815d1d3b2f96
Christian Adler <chris.adler@gmx.de>
2024-04-29 20:28:36 +0200

    improve phone controls

--- phoneControls.js
+++ phoneControls.js
@@ -1,13 +1,35 @@
 class PhoneControls {
-  constructor(props) {
+  constructor(canvas) {
+    this.canvas = canvas;
     this.tilt = 0;
+    this.forward = true;
+    this.reverse = false;
+    this.canvasAngle = 0;
     this.#addEventListeners();
 
   }
 
   #addEventListeners() {
-    window.addEventListener("deviceorientation", e => {
-      this.tilt = e.beta * Math.PI / 180;
+    // window.addEventListener("deviceorientation", e => {
+    //   this.tilt = e.beta * Math.PI / 180;
+    //   const canvasAngel = -this.tile;
+    //   this.canvas.style.transform = `translate(-50%,-50%) rotate(${canvasAngel}rad)`;
+    // });
+
+    window.addEventListener("devicemotion", e => {
+      this.tilt = Math.atan2(e.accelerationIncludingGravity.y, e.accelerationIncludingGravity.x);
+      const newCanvasAngel = -this.tilt;
+      this.canvasAngle = this.canvasAngle * 0.6 + newCanvasAngel * 0.4;
+      this.canvas.style.transform = `translate(-50%,-50%) rotate(${this.canvasAngle}rad)`;
+    });
+
+    window.addEventListener('touchstart', ev => {
+      this.reverse = true;
+      this.forward = false;
+    });
+    window.addEventListener('touchend', ev => {
+      this.reverse = false;
+      this.forward = true;
     });
   }
 }
